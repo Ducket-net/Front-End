c6464d9b5c7608d868211d3809e3759692c87b89
Wimell <heywimell@gmail.com>
2023-06-07 21:17:07 -0400

    Updated URL name

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,6 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  // add this section to your config file
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 });
