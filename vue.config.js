ed725b588fea25f676020add988dfe00e93d181e
Wimell <heywimell@gmail.com>
2023-06-07 21:26:26 -0400

    Contents

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,4 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // add this section to your config file
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 });
