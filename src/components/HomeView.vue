7e96d126a5834f7b6e82444532b2fbc872a5847d
Wimell <heywimell@gmail.com>
2023-06-07 21:01:05 -0400

    Build Updates

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,23 +1,22 @@
 <!-- src/components/Home.vue -->
 <template>
-    <div class="grid grid-cols-3">
-        <GameRoom :roomId="home" />
-        <GameRoom :roomId="cake" />
-    </div>
+  <div class="grid grid-cols-3">
+    <h1>Welcome to Suite... Coming soon</h1>
+  </div>
 </template>
 
 <script>
-import GameRoom from "./GameRoom.vue";
+// import GameRoom from "./GameRoom.vue";
 
 export default {
-    components: {
-        GameRoom,
-    },
-    data() {
-        return {
-            home: "home",
-            cake: "cake",
-        };
-    },
+  components: {
+    // GameRoom,
+  },
+  data() {
+    return {
+      home: "home",
+      cake: "cake",
+    };
+  },
 };
 </script>
