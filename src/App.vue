af8a51ab691268c117438fafc512609e4a1f46b4
Wimell <heywimell@gmail.com>
2023-06-09 09:28:34 -0400

    Display update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <!-- src/App.vue -->
 <template>
-  <div id="app" class="bg-gray-600 h-full w-full">
+  <div
+    id="app"
+    class="bg-gray-600 h-full w-full mx-auto md:max-w-sm md:p-1 md:rounded-lg md:mt-4"
+  >
     <router-view></router-view>
   </div>
 </template>
