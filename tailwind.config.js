4efaf5e90e10e8481cb3cb7eb108685c34b9bbfd
Wimell <heywimell@gmail.com>
2023-06-13 00:20:24 -0400

    Added Splash Screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 // tailwind.config.js
 module.exports = {
-    purge: {
-        enabled: process.env.NODE_ENV === "production",
-        content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    },
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "./index.html",
+      "./src/**/*.{vue,js,ts,jsx,tsx}",
+      "./splash.html",
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
