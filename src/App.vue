3261d6cc0ba5aed07db19703ba5ea6d3f8da15af
Wimell <heywimell@gmail.com>
2023-06-13 19:15:52 -0400

    Updated Splash

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,9 @@
         <!-- Splash Screen Image -->
 
         <div class="splash-screen-image">
+          <!-- Replace the hardcoded image path with the computed property 'splashScreenImage' -->
           <img
-            src="/splashscreens/apple-splash-1290-2796.png"
+            :src="splashScreenImage"
             class="fixed -top-12 bottom-0 left-0 right-0"
           />
         </div>
@@ -87,14 +88,149 @@ export default {
     catalogItems() {
       return this.$store.state.catalog || [];
     },
+    splashScreenImage() {
+      // Set the default splash screen image path
+      let imagePath = "/splashscreens/apple-splash-1284-2778.png";
+
+      // Get the device dimensions and pixel ratio
+      const deviceWidth = window.innerWidth;
+      const deviceHeight = window.innerHeight;
+      const pixelRatio = window.devicePixelRatio;
+
+      // Define the splash screens based on width, height, and pixel ratio
+      const splashScreens = [
+        {
+          path: "/splashscreens/apple-splash-2048-2732.png",
+          width: 1024,
+          height: 1366,
+          ratio: 2,
+        },
+        {
+          path: "/splashscreens/apple-splash-1668-2388.png",
+          width: 834,
+          height: 1194,
+          ratio: 2,
+        },
+        {
+          path: "/splashscreens/apple-splash-1536-2048.png",
+          width: 768,
+          height: 1024,
+          ratio: 2,
+        },
+        {
+          path: "/splashscreens/apple-splash-1668-2224.png",
+          width: 834,
+          height: 1112,
+          ratio: 2,
+        },
+        {
+          path: "/splashscreens/apple-splash-1620-2160.png",
+          width: 810,
+          height: 1080,
+          ratio: 2,
+        },
+        {
+          path: "/splashscreens/apple-splash-1290-2796.png",
+          width: 430,
+          height: 932,
+          ratio: 3,
+        },
+        {
+          path: "/splashscreens/apple-splash-1179-2556.png",
+          width: 393,
+          height: 852,
+          ratio: 3,
+        },
+        {
+          path: "/splashscreens/apple-splash-1284-2778.png",
+          width: 428,
+          height: 926,
+          ratio: 3,
+        },
+        {
+          path: "/splashscreens/apple-splash-1284-2778.png",
+          width: 428,
+          height: 926,
+          ratio: 2,
+        },
+        {
+          path: "/splashscreens/apple-splash-1170-2532.png",
+          width: 390,
+          height: 844,
+          ratio: 3,
+        },
+        {
+          path: "/splashscreens/apple-splash-1125-2436.png",
+          width: 375,
+          height: 812,
+          ratio: 3,
+        },
+        {
+          path: "/splashscreens/apple-splash-1242-2688.png",
+          width: 414,
+          height: 896,
+          ratio: 3,
+        },
+        {
+          path: "/splashscreens/apple-splash-828-1792.png",
+          width: 414,
+          height: 896,
+          ratio: 2,
+        },
+        {
+          path: "/splashscreens/apple-splash-1242-2208.png",
+          width: 414,
+          height: 736,
+          ratio: 3,
+        },
+        {
+          path: "/splashscreens/apple-splash-750-1334.png",
+          width: 375,
+          height: 667,
+          ratio: 2,
+        },
+        {
+          path: "/splashscreens/apple-splash-640-1136.png",
+          width: 320,
+          height: 568,
+          ratio: 2,
+        },
+      ];
+
+      // Find the appropriate splash screen based on the device dimensions and pixel ratio
+      for (const ss of splashScreens) {
+        if (
+          deviceWidth === ss.width &&
+          deviceHeight === ss.height &&
+          pixelRatio === ss.ratio
+        ) {
+          imagePath = ss.path;
+          break;
+        }
+      }
+
+      return imagePath;
+    },
   },
   methods: {
+    preloadImage(imagePath) {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = imagePath;
+        img.onload = () => {
+          resolve(imagePath);
+        };
+        img.onerror = () => {
+          reject();
+        };
+      });
+    },
     initApp() {
       // Data fetching or processing can go here, for example
 
       setTimeout(() => {
         this.showSplashScreen = false;
-      }, 2000); // You can adjust the delay as needed
+      }, 1200); // You can adjust the delay as needed
     },
     checkIfPwa() {
       if (
@@ -112,7 +248,9 @@ export default {
     this.checkIfPwa();
 
     if (this.checkIfPwa()) {
-      this.initApp();
+      this.preloadImage(this.splashScreenImage).then(() => {
+        this.initApp();
+      });
     } else {
       this.showSplashScreen = false;
     }
