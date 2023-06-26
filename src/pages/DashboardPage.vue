85e586e78d0908a826d908afec496b82f217b1ca
Username <hello@example.com>
2023-06-26 19:38:18 -0400

    Locked App

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -9,20 +9,35 @@
     </div>
     <GameRoom roomId="home" size="small" />
 
+    <div
+      class="shadow-xl relative -top-32 rounded-full bg-red-500 p-[3px] grid grid-cols-[30px,auto] max-w-[200px] mx-auto mt-4 bg-gradient-to-bl from-red-400 to-red-800"
+    >
+      <div
+        class="bg-gray-300 rounded-l-full border-2 border-gray-300 flex items-baseline"
+      >
+        <div
+          class="bg-white h-6 w-6 rounded-full m-auto overflow-hidden border-2 border-white"
+        >
+          <img src="icons/frank.png" class="object-none" />
+        </div>
+      </div>
+      <div class="bg-white rounded-r-full text-sm pl-2 p-1">Coming Soon!</div>
+    </div>
+
     <div class="p-4 grid grid-flow-row gap-4">
-      <SpecialCard @card-click="createPage">
+      <!-- <SpecialCard @card-click="createPage">
         <h2 class="font-bold text-sm">
           <font-awesome-icon :icon="['fas', 'square']" />
           Create
         </h2>
-      </SpecialCard>
+      </SpecialCard> -->
       <SpecialCard @card-click="authorize">
         <h2 class="font-bold text-sm">
           <font-awesome-icon :icon="['fas', 'lock']" />
           Login with Ducket
         </h2>
       </SpecialCard>
-
+      <!-- 
       <SpecialCard>
         <h2 class="font-bold text-sm">
           <font-awesome-icon :icon="['fas', 'arrow-up-from-bracket']" />
@@ -34,7 +49,7 @@
         <h2 class="font-bold text-sm">
           <font-awesome-icon :icon="['fas', 'bug']" /> Report Bugs & Issues
         </h2>
-      </SpecialCard>
+      </SpecialCard> -->
     </div>
   </div>
 </template>
