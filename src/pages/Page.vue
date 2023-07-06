99ac0aaa2f74518465dc89af6ee1a02ecb4a3a03
Username <hello@example.com>
2023-07-05 23:41:31 -0400

    Page Builder test

--- src/pages/Page.vue
+++ src/pages/Page.vue
@@ -1,13 +1,37 @@
-<script setup>
-defineProps({ blok: Object });
-</script>
-
 <template>
-  <div v-editable="blok" class="px-6">
-    <StoryblokComponent
-      v-for="inblok in blok.body"
-      :blok="inblok"
-      :key="inblok._uid"
-    />
-  </div>
+  <RenderContent
+    v-if="canShowContent"
+    model="page"
+    :content="content"
+    :api-key="apiKey"
+  />
 </template>
+
+<script>
+// OR use this one if you are on Vue 3
+import {
+  getContent,
+  RenderContent,
+  isPreviewing,
+} from '@builder.io/sdk-vue/vue3';
+
+export default {
+  data: () => ({
+    canShowContent: false,
+    content: null,
+    apiKey: '2cf1663ebfe04c178f9547e2f5e7eb28',
+  }),
+  mounted() {
+    getContent({
+      model: 'page',
+      apiKey: '2cf1663ebfe04c178f9547e2f5e7eb28', // <-- Replace with your Public API Key
+      userAttributes: {
+        urlPath: window.location.pathname,
+      },
+    }).then((res) => {
+      this.content = res;
+      this.canShowContent = this.content || isPreviewing();
+    });
+  },
+};
+</script>
