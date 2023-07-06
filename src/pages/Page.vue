a41e26c819abea10924374573785840ae0f36de8
Username <hello@example.com>
2023-07-05 23:43:16 -0400

    Page Builder test

--- src/pages/Page.vue
+++ src/pages/Page.vue
@@ -3,11 +3,17 @@
     v-if="canShowContent"
     model="page"
     :content="content"
-    :api-key="2cf1663ebfe04c178f9547e2f5e7eb28"
+    :api-key="apiKey"
+    :customComponents="getRegisteredComponents()"
   />
 </template>
-
 <script>
+// use this if you are on Vue 2
+import {
+  getContent,
+  RenderContent,
+  isPreviewing,
+} from '@builder.io/sdk-vue/vue2';
 // OR use this one if you are on Vue 3
 import {
   getContent,
@@ -16,6 +22,7 @@ import {
 } from '@builder.io/sdk-vue/vue3';
 
 export default {
+  components: { RenderContent },
   data: () => ({
     canShowContent: false,
     content: null,
@@ -24,7 +31,7 @@ export default {
   mounted() {
     getContent({
       model: 'page',
-      apiKey: '2cf1663ebfe04c178f9547e2f5e7eb28', // <-- Replace with your Public API Key
+      apiKey: '2cf1663ebfe04c178f9547e2f5e7eb28',
       userAttributes: {
         urlPath: window.location.pathname,
       },
